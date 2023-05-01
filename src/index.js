59264859731f11b9b5578f949f6ff29fb791c4d2
user.email <developerjenzii@gmail.com>
2023-05-02 00:11:59 +0200

    install req to run npm start

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import css from "./file.css";
 
 function component() {
   const div = document.createElement('div');
+  div.innerHTML = 'hello im an entry to this app'
   div.id = 'mainComponent';
   document.body.appendChild(div);
 }
