976def38a7a96cff721d0099887795bf757c6668
user.email <developerjenzii@gmail.com>
2023-05-03 21:07:50 +0200

    add html fluff for user interaction

--- src/index.js
+++ src/index.js
@@ -2,10 +2,19 @@ import { ToDoItem } from "./ToDoItem";
 import css from "./file.css";
 
 function component() {
+  const date = new Date().toLocaleDateString('en-CA');
   const div = document.createElement('div');
   div.id = 'mainComponent';
   div.innerHTML = ` <div id="add-task"> 
-  <input type="text" placeholder="Insert future accomplishment"> 
+  <input type="text" id="input-title" placeholder="Insert future accomplishment"> 
+  <input type="text" id="input-description" placeholder="Insert future accomplishment description">
+  <input type="date" id="input-date" value="${date}">  
+  <select id = "input-dropdown">
+<option> Priority </option>
+<option> A ~ ASAP </option>
+<option> B ~ this week </option>
+<option> C ~ fun </option>
+</select>
   <button id="add-task-button">Lets go</button>
 </div> `
 document.body.appendChild(div);
