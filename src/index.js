7fdc9e4c662ab6fb52b92452598df271bcb31a20
user.email <developerjenzii@gmail.com>
2023-05-07 16:22:52 +0200

    add new functionality to create form dynamically

--- src/index.js
+++ src/index.js
@@ -1,26 +1,15 @@
 import { ToDoItem } from "./ToDoItem";
 import css from "./file.css";
+import { createForm } from "./view";
+import { button, createDiv } from "./util";
+const createForms = '"./view"';
 
 function component() {
-  const date = new Date().toLocaleDateString('en-CA');
+  
   const div = document.createElement('div');
   div.id = 'mainComponent';
-  div.innerHTML = ` <div id="add-task">
-  <div id="container-1">
-  <input type="text" class="input" id="input-title" placeholder="Insert future accomplishment"> 
-  <input type="text" class="input" id="input-description" placeholder="Insert future accomplishment description">
-  <input type="date" class="input" id="input-date" value="${date}">  
-  </div>
-  <div id="container-2">
-  <select class="input" id = "input-dropdown">
-<option> Priority </option>
-<option> A ~ ASAP </option>
-<option> B ~ this week </option>
-<option> C ~ 4 fun </option>
-</select>
-  <button id="add-task-button">Lets go</button>
-  </div>
-</div> `
+  div.innerHTML =  createForm();
+  
 document.body.appendChild(div);
 }
 
@@ -34,8 +23,9 @@ function getInputColl(...args) {
  }
  
 component();
-const button = document.getElementById('add-task-button');
-button.addEventListener('click', () => { let inputTitle = document.getElementById('input-title')
-  let item = new ToDoItem(...(getInputColl('0', '1', '2', '3')))
-item.createUI()  } );
 
+createDiv('','form');
+createDiv('form','container-1',  ['input,input-title,input,text,Insert future accomplishment', 
+                                    'input,input-title,input,text,Insert future accomplishment',
+                                    'input,input-title,input,text,Insert future accomplishment']);
+ createDiv('form','container-2',['select,input,input-dropdown']);
