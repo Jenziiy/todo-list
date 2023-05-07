bc4703db5b639e0dfd9159f93d1d4c2a8a7bd8b4
user.email <developerjenzii@gmail.com>
2023-05-07 02:46:28 +0200

    change layout css

--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@ function component() {
   const date = new Date().toLocaleDateString('en-CA');
   const div = document.createElement('div');
   div.id = 'mainComponent';
-  div.innerHTML = ` <div id="add-task"> 
+  div.innerHTML = ` <div id="add-task">
+  <div id="container-1">
   <input type="text" class="input" id="input-title" placeholder="Insert future accomplishment"> 
   <input type="text" class="input" id="input-description" placeholder="Insert future accomplishment description">
   <input type="date" class="input" id="input-date" value="${date}">  
+  </div>
+  <div id="container-2">
   <select class="input" id = "input-dropdown">
 <option> Priority </option>
 <option> A ~ ASAP </option>
@@ -16,6 +19,7 @@ function component() {
 <option> C ~ 4 fun </option>
 </select>
   <button id="add-task-button">Lets go</button>
+  </div>
 </div> `
 document.body.appendChild(div);
 }
