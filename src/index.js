c295dfef6426477327ea0d3d739a6f67d098f71c
user.email <developerjenzii@gmail.com>
2023-05-08 23:23:55 +0200

    add changes to dynamic build of page

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import css from "./file.css";
-import { createForm } from "./view";
-
+import { createForm, createPageStructure } from "./view";
 function component() {
   
   const div = document.createElement('div');
   div.id = 'mainComponent';
-  div.innerHTML =  createForm(); 
-document.body.appendChild(div);
+  createPageStructure();
+  const mainWrapper = document.getElementById('main-wrapper');
+  const form = document.getElementById('add-task');
+  mainWrapper.appendChild(form);
 }
 
 function getInputColl(...args) {
--- src/view.js
+++ src/view.js
@@ -7,18 +7,35 @@ const htmlPageStructure = {
   footer: document.createElement('footer'),
 };
 
-for ( const prop in htmlPageStructure ) {
-  let element = htmlPageStructure[prop];
-  element.classList.add('page-sections');
-  document.body.appendChild(element);
- let childWrapper = document.createElement('div');
- childWrapper.setAttribute(`id`, `${prop}-wrapper`);
- element.appendChild(childWrapper);
-  
+export function createPageStructure(){
+  for ( const prop in htmlPageStructure ) {
+    let element = htmlPageStructure[prop];
+    element.classList.add('page-sections');
+    document.body.appendChild(element);
+  let childWrapper = document.createElement('div');
+  childWrapper.setAttribute(`id`, `${prop}-wrapper`);
+  element.appendChild(childWrapper);
+  }
 }
 
 export function createForm(){
-  return `<div id="add-task">
+  let form = document.createElement('form');
+  form.id = 'add-task';
+  form.setAttribute("method","post");
+  form.setAttribute("action", "submit");
+  form.appendChild(createFormInput('Task: ', 'What you gonna do?'));
+  form.appendChild(createFormInput('Description: ', 'pLz eLaBoRaTe?'));
+};
+
+function createFormInput(labelName, placeholder){
+  let label = document.createElement('label');
+  label.innerText = labelName;
+  let input = document.createElement('input');
+  input.setAttribute(`placeholder`, placeholder);
+  input.appendChild(label);
+}
+
+   `<div id="add-task">
   <div id="container-1">
     <input type="text" class="input" id="input-title" placeholder="Insert future accomplishment"> 
     <input type="text" class="input" id="input-description" placeholder="Insert future accomplishment description">
@@ -34,4 +51,3 @@ export function createForm(){
     <button id="add-task-button">Lets go</button>
   </div>
 </div> `
-}
