c5cefa21b93572a8ccd5aefb4cd7638fe80a5f67
user.email <developerjenzii@gmail.com>
2023-05-09 23:06:17 +0200

    Create inline logic for toDoItem

--- src/ToDoItem.js
+++ src/ToDoItem.js
@@ -6,23 +6,26 @@ import { date } from "./util";
 export class ToDoItem {
 
   constructor(title, description, dueDate, priority) {
-    this.title = title ? title : 'ok';
-    this.description = description ? description : 'ok';
-    this.dueDate = dueDate ? dueDate : 'ok';
+    this.title = title ? title : 'Missing title';
+    this.description = description ? description : 'Missing description';
+    this.dueDate = dueDate;
     this.priority = priority ? priority : 'A';
   }
 
-  createUI(){
-     if (this.dueDate != undefined) { 
+  createDateFormat(){
+    if (this.dueDate != undefined) { 
       let dateArr = this.dueDate.toString().split('-'); 
-      this.dateString = `${dateArr[2]}-${dateArr[1]}-${dateArr[0]}`; 
+      return this.dateString = `${dateArr[2]}-${dateArr[1]}-${dateArr[0]}`; 
       }
+  }
+
+  createUI(){
     const div = document.createElement('div');
     div.id = 'todo-item';
     div.innerHTML = `
     <p> ${this.title ? this.title : 'Missing title'} </p>
     <p>${this.description ? this.description : 'Missing description'}</p>
-    <p>${ this.dateString} </p>
+    <p>${ this.createDateFormat()} </p>
     <p>${this.priority[0] == 'A' ||
          this.priority[0] == 'B' ||
          this.priority[0] == 'C' ? this.priority : 'Missing priority'}</p>
