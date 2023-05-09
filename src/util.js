cbdc8cba31710f8bb7a733a59b9533dd814f38ba
user.email <developerjenzii@gmail.com>
2023-05-09 23:24:42 +0200

    refactor code and clean import utils

--- src/util.js
+++ src/util.js
@@ -1,19 +1,17 @@
 import { ToDoItem } from "./ToDoItem";
-import { createForm } from "./view";
 
 export function el(id) { return document.getElementById(id); }
 
 export function getInputColl(...args) {
   const valueArr = [];
     for( let arg of args ){
-    let value = document.getElementsByClassName('input')[arg].value;
-    valueArr.push(value);
+    valueArr.push(document.getElementsByClassName('input')[arg].value);
   }
   return valueArr;
  }
 
   export function buttonLogic(){
-    const button = document.getElementById('add-task-button');
-    return button.addEventListener('click', () => { console.log('btn');let item = new ToDoItem(...(getInputColl('0', '1', '2')))
-                          item.createUI()  })};
+    return el('add-task-button').addEventListener('click', () => { new ToDoItem(...(
+                                    getInputColl('0', '1', '2')))
+                                    .createUI()  })};
 
