954bb0816d6a55441765a727248b0d65a6ea6542
user.email <developerjenzii@gmail.com>
2023-05-12 23:12:22 +0200

    re-entered module to get rid of error

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import css from "./file.css";
 import { createPage } from "./view";
-import { Model } from "./Models/TaskListModel";
 import { View } from "./view";
 import { Controller } from "./Controller";
+import { Model } from "./Models/taskListModel";
 
 function component() {
    createPage();
