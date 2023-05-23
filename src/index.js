b8e4998b898e440727bbb302e3c5cd12d5a7f89f
user.email <developerjenzii@gmail.com>
2023-05-23 23:49:03 +0200

    fix error by importing right n frm clss

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-import { Controller } from "./Controller";
+import app from "./Controller";
 import css from "./file.css";
 import model from "./Models/ObjectModel";
 import view from "./view";
 
 app;
-view.createPage();
